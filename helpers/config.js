c9c046954c3fdf41dbbcb1ce7949b61ebf62d460
hotam-singh <hotam@websitetoolbox.com>
2017-09-26 19:23:35 +0530

    made initial commit.

---
+++ helpers/config.js
@@ -0,0 +1,52 @@
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+var z_schema = require('./z_schema.js');
+var configSchema = require('../schema/config.js');
+var constants = require('../helpers/constants.js');
+
+/**
+ * Loads config.json file
+ * @memberof module:helpers
+ * @implements {validateForce}
+ * @param {string} configPath
+ * @returns {Object} configData
+ */
+function Config (configPath) {
+	var configData = fs.readFileSync(path.resolve(process.cwd(), (configPath || 'config.json')), 'utf8');
+	if (!configData.length) {
+		process.exit(1);
+	} else {
+		configData = JSON.parse(configData);
+	}
+
+	var validator = new z_schema();
+	var valid = validator.validate(configData, configSchema.config);
+
+	if (!valid) {
+		process.exit(1);
+	} else {
+		validateForce(configData);
+		return configData;
+	}
+}
+
+/**
+ * Validates nethash value from constants and sets forging force to false if any.
+ * @private
+ * @param {Object} configData
+ */
+function validateForce (configData) {
+	if (configData.forging.force) {
+		var index = constants.nethashes.indexOf(configData.nethash);
+
+		if (index !== -1) {
+			console.log('Forced forging disabled for nethash', configData.nethash);
+			configData.forging.force = false;
+		}
+	}
+}
+
+// Exports
+module.exports = Config;
