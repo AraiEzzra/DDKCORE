116e7ee86d22325dbfb3a65c166527b071281b24
navin <navin@navin.purohit@oodlestechnologies.com>
2018-03-07 12:00:26 +0530

    resolve parsing error on frontend

--- public/js/controllers/existingETPSUserController.js
+++ public/js/controllers/existingETPSUserController.js
@@ -30,70 +30,34 @@ angular.module('ETPApp').controller('existingETPSUserController', ['$scope', '$r
         return btoa(dynamic_key);
     }
 
-    // function to validate existing ETPS user
+    // function to validate existing ETPS user from ETP_test database
     $scope.validateExistingUser = function (username, password) {
-        var api_key = this.generateApiKey($scope.API_KEY_GLOBAL);
-        var url = $scope.URL_GLOBAL + "users/login.php?key=" + api_key;
-        var post = "username=" + btoa(username) + "&password=" + btoa(password);
-        $http({
-            method: 'POST',
-            cache: false,
-            url: url,
-            data: post,
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
-        })
-        .success(function (resp) {
-            if (!resp.records) {
-                $scope.errorMessage = resp.message;
-            } else {
-                var userInfo = {};
-                Object.assign(userInfo, resp.records);
-                $http.post("/api/accounts/existingETPSUser", { userInfo: userInfo }).then(function (response) {
-                    if (response.data.success) {
-                        if (response.data.isMigrated) {
-                            $scope.errorMessage = 'User is already migrated';
-                        } else {
-                            $scope.newUser(userInfo);
-                        }
-                    } else {
-                        $scope.errorMessage = response.data.error;
-                    }
-                });
-            }
-        })
-        .error(function (err) {
-            $scope.errorMessage = err;
-        });
-    } */
 
-    //Navin: function to validate existing ETPS user from ETP_test database
-    $scope.validateExistingUser = function (username, password) {
-        
         $http.post("/api/accounts/existingETPSUser/validate", {
             username: btoa(username),
             password: btoa(password)
-        }).success(function (resp) {
-            console.log('resp : ' + JSON.stringify(resp));
-            if (!resp.records) {
-                $scope.errorMessage = resp.message;
-            } else {
-                var userInfo = {};
-                Object.assign(userInfo, resp.records);
-                $http.post("/api/accounts/existingETPSUser", { userInfo: userInfo }).then(function (response) {
-                    console.log('response : ' + JSON.stringify(response));
-                    if (response.data.success) {
-                        if (response.data.isMigrated) {
-                            $scope.errorMessage = 'User is already migrated';
+        })
+            .success(function (resp) {
+                if (!resp.records) {
+                    $scope.errorMessage = resp.message;
+                } else {
+                    var userInfo = {};
+                    Object.assign(userInfo, resp.records);
+                    $http.post("/api/accounts/existingETPSUser", { userInfo: userInfo }).then(function (response) {
+                        if (response.data.success) {
+                            if (response.data.isMigrated) {
+                                $scope.errorMessage = 'User is already migrated';
+                            } else {
+                                $scope.newUser(userInfo);
+                            }
                         } else {
-                            $scope.newUser(userInfo);
+                            $scope.errorMessage = response.data.error;
                         }
-                    } else {
-                        $scope.errorMessage = response.data.error;
-                    }
-                });
-            }
-        }).error(function (err) {
-            $scope.errorMessage = err;
-        });
+                    });
+                }
+            })
+            .error(function (err) {
+                $scope.errorMessage = err;
+            });
     }
 }]);
