7ea3a524607fa5e190f2ce3c9638107c7776fa5e
hotam-singh <hotam.hbti123@gmail.com>
2018-07-06 12:53:57 +0530

    replaced dist folder to fix swagger issue

--- dist/lib/object-assign-pollyfill.js
+++ dist/lib/object-assign-pollyfill.js
@@ -1,23 +1,23 @@
 if (typeof Object.assign != 'function') {
-	(function () {
-		Object.assign = function (target) {
-			
-			if (target === undefined || target === null) {
-				throw new TypeError('Cannot convert undefined or null to object');
-			}
+  (function () {
+    Object.assign = function (target) {
+      'use strict';
+      if (target === undefined || target === null) {
+        throw new TypeError('Cannot convert undefined or null to object');
+      }
 
-			let output = Object(target);
-			for (let index = 1; index < arguments.length; index++) {
-				let source = arguments[index];
-				if (source !== undefined && source !== null) {
-					for (let nextKey in source) {
-						if (Object.prototype.hasOwnProperty.call(source, nextKey)) {
-							output[nextKey] = source[nextKey];
-						}
-					}
-				}
-			}
-			return output;
-		};
-	})();
+      var output = Object(target);
+      for (var index = 1; index < arguments.length; index++) {
+        var source = arguments[index];
+        if (source !== undefined && source !== null) {
+          for (var nextKey in source) {
+            if (Object.prototype.hasOwnProperty.call(source, nextKey)) {
+              output[nextKey] = source[nextKey];
+            }
+          }
+        }
+      }
+      return output;
+    };
+  })();
 }
