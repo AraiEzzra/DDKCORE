39658ceb0b112a9ef8c7fa51cf4469c510d695b4
satishjoshi95 <satish.joshi@oodlestechnologies.com>
2018-07-19 17:46:13 +0530

    Referral Stats sponsor list

--- helpers/referal.js
+++ helpers/referal.js
@@ -65,4 +65,75 @@ module.exports.api = function (app) {
         });
     });
 
+    app.post('/referral/list', function (req, res) {
+
+        let hierarchy = {};
+
+        let params = [],
+            sponsorsList = [],
+            level = 1;
+
+        function arrayPush(resp) {
+            for (let i = 0; i < resp.length; i++) {
+                params.push('$' + (i + 1));
+                sponsorsList.push(resp[i].address);
+            }
+        }
+
+        function findSponsors(params, arr, cb) {
+            if (level <= 15) {
+                library.db.query('SELECT address from referals WHERE level[1] IN (' + params.join(',') + ')', arr)
+                    .then(function (resp) {
+                        params.length = 0;
+                        sponsorsList.length = 0;
+                        if (resp.length) {
+                            (async function () {
+                                await arrayPush(resp);
+                            }());
+
+                            hierarchy[level] = JSON.parse(JSON.stringify(sponsorsList));
+                            level++;
+                            findSponsors(params, sponsorsList, cb);
+                        }
+                        if (params.length == 0) {
+                            return setImmediate(cb, null);
+                        }
+                    });
+            } else {
+                return setImmediate(cb, null);
+            }
+        }
+
+        library.db.query('SELECT address from referals WHERE level[1] = ${address}', {
+            address: req.body.referrer_address
+        }).then(function (resp) {
+            if (resp.length) {
+                (async function () {
+                    await arrayPush(resp);
+                }());
+
+                hierarchy[level] = JSON.parse(JSON.stringify(sponsorsList));
+                level++;
+                findSponsors(params, sponsorsList, function (err) {
+                    if (err) {
+                        return res.status(400).json({
+                            success: false,
+                            error: err
+                        });
+                    }
+                    return res.status(200).json({
+                        success: true,
+                        info: hierarchy
+                    });
+                });
+            } else {
+                return res.status(200).json({
+                    success: true,
+                    info: "No Sponsor Found"
+                });
+            }
+        });
+
+    });
+
 }
