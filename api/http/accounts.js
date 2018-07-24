f8169a72fc36a19e1412ff84c6a0ef58a959a4a9
Satish Joshi <39725177+satishjoshi95@users.noreply.github.com>
2018-07-24 17:03:27 +0530

    Update accounts.js

--- api/http/accounts.js
+++ api/http/accounts.js
@@ -78,7 +78,8 @@ function AccountsHttpApi (accountsModule, app) {
 		'get /getWithdrawlStatus': 'getWithdrawlStatus',
 		'post /sendWithdrawlAmount': 'sendWithdrawlAmount',
 		'post /enablePendingGroupBonus': 'enablePendingGroupBonus',
-		'get /generatenpNewPassphase':'generatenpNewPassphase'
+		'get /generatenpNewPassphase':'generatenpNewPassphase',
+		'post /forgotEtpsPassword': 'forgotEtpsPassword'
 	});
 
 	if (process.env.DEBUG && process.env.DEBUG.toUpperCase() === 'TRUE') {
