29a4597befb260a4da26d761b3129af68a70673a
navin <navin@navin.purohit@oodlestechnologies.com>
2018-08-17 15:57:04 +0530

    rewrite comment on migration logic file

--- logic/Migration.js
+++ logic/Migration.js
@@ -7,22 +7,22 @@ let sql = require('../sql/referal_sql');
 let modules, library, self;
 
 /**
- * Main transfer logic.
- * @memberof module:transactions
+ * Main migration logic.
+ *
  * @class
- * @classdesc Main transfer logic.
+ * @classdesc Main migration logic.
  */
 // Constructor
 function Migration(logger, db, cb) {
-    self = this;
-    self.scope = {
-        logger: logger,
-        db: db
-    };
+	self = this;
+	self.scope = {
+		logger: logger,
+		db: db
+	};
 
-    if (cb) {
-        return setImmediate(cb, null, this);
-    }
+	if (cb) {
+		return setImmediate(cb, null, this);
+	}
 }
 
 // Public methods
@@ -39,47 +39,38 @@ Migration.prototype.bind = function (accounts, rounds) {
 };
 
 /**
- * Assigns data to transaction recipientId and amount.
+ * Assigns data to transaction groupBonus, senderId, stakedAmount, public key and name.
  * @param {Object} data
  * @param {transaction} trs
  * @return {transaction} trs with assigned data
  */
 Migration.prototype.create = function (data, trs) {
-    trs.groupBonus = data.groupBonus;
-    trs.senderId = data.sender.address;
-    trs.stakedAmount = data.totalFrozeAmount;
-    // trs.balance = data.amount;
-    trs.publicKey = data.sender.publicKey;
+	trs.groupBonus = data.groupBonus;
+	trs.senderId = data.sender.address;
+	trs.stakedAmount = data.totalFrozeAmount;
+	// trs.balance = data.amount;
+	trs.publicKey = data.sender.publicKey;
 	trs.trsName = "Migration";
 	return trs;
 };
 /**
- * Returns send fees from constants.
+ * Returns fees.
  * @param {transaction} trs
  * @param {account} sender
  * @return {number} fee
  */
 Migration.prototype.calculateFee = function (trs, sender) {
 
-    return 0;
+	return 0;
 };
 
 /**
- * Verifies recipientId and amount greather than 0.
  * @param {transaction} trs
  * @param {account} sender
  * @param {function} cb
  * @return {setImmediateCallback} errors | trs
  */
 Migration.prototype.verify = function (trs, sender, cb) {
-	/* if (!trs.recipientId) {
-		return setImmediate(cb, 'Missing recipient');
-	}
-
-	if (trs.amount <= 0) {
-		return setImmediate(cb, 'Invalid transaction amount');
-	} */
-
 	return setImmediate(cb, null, trs);
 };
 
@@ -102,11 +93,7 @@ Migration.prototype.getBytes = function (trs) {
 };
 
 /**
- * Calls setAccountAndGet based on transaction recipientId and
- * mergeAccountAndGet with unconfirmed trs amount.
- * @implements {modules.accounts.setAccountAndGet}
- * @implements {modules.accounts.mergeAccountAndGet}
- * @implements {modules.rounds.calc}
+ * Calls db query to insert data into mem_Accounts table.
  * @param {transaction} trs
  * @param {block} block
  * @param {account} sender
@@ -114,45 +101,25 @@ Migration.prototype.getBytes = function (trs) {
  * @return {setImmediateCallback} error, cb
  */
 Migration.prototype.apply = function (trs, block, sender, cb) {
-/* 	modules.accounts.setAccountAndGet({address: trs.recipientId}, function (err, recipient) {
-		if (err) {
-			return setImmediate(cb, err);
-		}
-
-		modules.accounts.mergeAccountAndGet({
-			address: trs.address,
-			balance: trs.balance,
-			u_balance: trs.balance,
-			blockId: block.id,
-			round: modules.rounds.calc(block.height)
-		}, function (err) {
-			return setImmediate(cb, err);
-		});
-	}); */
 
 	self.scope.db.none(sql.insertMemberAccount, {
-        address: trs.senderId,
-        publicKey: trs.publicKey,
-        balance: trs.stakedAmount,
-        u_balance: trs.stakedAmount,
-        totalFrozeAmount: trs.stakedAmount,
-        group_bonus: trs.groupBonus
-    }).then(function () {
-        cb(null);
-    }).catch(function (err) {
-		if(err){
-			console.log("err : "+err)
+		address: trs.senderId,
+		publicKey: trs.publicKey,
+		balance: trs.stakedAmount,
+		u_balance: trs.stakedAmount,
+		totalFrozeAmount: trs.stakedAmount,
+		group_bonus: trs.groupBonus
+	}).then(function () {
+		cb(null);
+	}).catch(function (err) {
+		if (err) {
+			console.log("error : " + err)
 		}
-        cb(err);
-    });
+		cb(err);
+	});
 };
 
 /**
- * Calls setAccountAndGet based on transaction recipientId and
- * mergeAccountAndGet with unconfirmed trs amount and balance negative.
- * @implements {modules.accounts.setAccountAndGet}
- * @implements {modules.accounts.mergeAccountAndGet}
- * @implements {modules.rounds.calc}
  * @param {transaction} trs
  * @param {block} block
  * @param {account} sender
@@ -160,20 +127,7 @@ Migration.prototype.apply = function (trs, block, sender, cb) {
  * @return {setImmediateCallback} error, cb
  */
 Migration.prototype.undo = function (trs, block, sender, cb) {
-	modules.accounts.setAccountAndGet({address: trs.recipientId}, function (err, recipient) {
-		if (err) {
-			return setImmediate(cb, err);
-		}
-		modules.accounts.mergeAccountAndGet({
-			address: trs.recipientId,
-			balance: -trs.amount,
-			u_balance: -trs.amount,
-			blockId: block.id,
-			round: modules.rounds.calc(block.height)
-		}, function (err) {
-			return setImmediate(cb, err);
-		});
-	});
+	return setImmediate(cb);
 };
 
 /**
@@ -219,33 +173,16 @@ Migration.prototype.dbRead = function (raw) {
  * @return {null}
  */
 Migration.prototype.dbSave = function (trs) {
-	/* return {
-		table: this.dbTable,
-		fields: this.dbFields,
-		values: {
-			gbAmount: trs.groupBonus,
-			transactionId: trs.id
-		}
-	}; */
 	return null;
 };
 
 /**
- * Checks sender multisignatures and transaction signatures.
  * @param {transaction} trs
  * @param {account} sender
- * @return {boolean} True if transaction signatures greather than 
- * sender multimin or there are not sender multisignatures.
+ * @return null
  */
 Migration.prototype.ready = function (trs, sender) {
-	if (Array.isArray(sender.multisignatures) && sender.multisignatures.length) {
-		if (!Array.isArray(trs.signatures)) {
-			return false;
-		}
-		return trs.signatures.length >= sender.multimin;
-	} else {
-		return true;
-	}
+	return null;
 };
 
 // Export
