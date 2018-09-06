bf42deee4f57cccfbce4be056beda02c4391340c
Satish Joshi <39725177+satishjoshi95@users.noreply.github.com>
2018-09-06 16:48:39 +0530

    Update accountCreateETPS.js

--- helpers/accountCreateETPS.js
+++ helpers/accountCreateETPS.js
@@ -366,7 +366,7 @@ function etpsMigrationProcess() {
                                                 }
                                                 callback();
                                             });
-                                        }, 350);
+                                        }, 400);
 
                                     } else {
                                         callback();
@@ -437,7 +437,7 @@ function etpsMigrationProcess() {
                                                 }
                                                 callback();
                                             });
-                                        }, 350);
+                                        }, 400);
 
                                     } else {
                                         callback();
