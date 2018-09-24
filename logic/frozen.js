afbb762da17cbaacc00e27b600363d5769888706
yarphen <sheremet.mykhaylo@gmail.com>
2018-09-24 15:09:25 +0300

    Fix to avoid negative fee

--- logic/frozen.js
+++ logic/frozen.js
@@ -322,7 +322,10 @@ Frozen.prototype.verify = function (trs, sender, cb) {
  * @return % based on amount
  */
 Frozen.prototype.calculateFee = function (trs, sender) {
-	return trs.stakedAmount > 0 ? (trs.stakedAmount * constants.fees.froze) / 100 : 0;
+	if (trs.stakedAmount < 0) {
+		return 0;
+	}
+	return (trs.stakedAmount * constants.fees.froze) / 100;
 };
 
 /**
