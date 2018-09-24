75986167864347b247fe633b90f260528f873c40
yarphen <sheremet.mykhaylo@gmail.com>
2018-09-24 12:31:35 +0300

    Cleanup

--- logic/frozen.js
+++ logic/frozen.js
@@ -395,7 +395,6 @@ Frozen.prototype.checkFrozeOrders = async function (sender) {
 		Promise.all(orders.map(order => updateOrderAndSendReward(order)));
 
 	const VOTE_COUNT_LIMIT = 3; // TODO: restore 27
-	const VOTE_COUNT_LIMIT = 2;
 
 	const disableFrozeOrder = async (order) => {
 		const secret = 'obvious illness service health witness useful correct brave asthma food install next';
@@ -416,8 +415,8 @@ Frozen.prototype.checkFrozeOrders = async function (sender) {
 	const unstakeTransactions = await disableFrozeOrders(freezeOrders.filter(o => o.voteCount >= VOTE_COUNT_LIMIT));
 
 	const res = [...rewardTransactions, ...unstakeTransactions].filter(t => !!t);
-        console.log(res);
-        return res;
+    console.log(res);
+    return res;
 };
 
 /**
