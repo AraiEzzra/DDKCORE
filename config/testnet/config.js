4cc5cbd7fc04c65376a4ef294caa162316ac2626
Aleksandr Shevchenko <axelsheva94@gmail.com>
2018-12-26 17:58:02 +0200

    Change 'true' to 'TRUE'

--- config/testnet/config.js
+++ config/testnet/config.js
@@ -94,7 +94,7 @@ module.exports = {
         }
     },
     loading: {
-        verifyOnLoading: env.VERIFY_ON_LOADING === 'true',
+        verifyOnLoading: env.VERIFY_ON_LOADING === 'TRUE',
         loadPerIteration: 5000
     },
     ssl: {
