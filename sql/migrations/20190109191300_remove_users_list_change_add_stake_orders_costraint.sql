115a6c95aa08c9ff3d192e23164c0ea72c31d296
Dmytro Mehed <d.mehed@sk-consulting.com.ua>
2019-01-09 20:03:33 +0200

    stake_orders_fix: fix constraint on stake_orders

---
+++ sql/migrations/20190109191300_remove_users_list_change_add_stake_orders_costraint.sql
@@ -0,0 +1,10 @@
+BEGIN;
+
+DROP VIEW IF EXISTS users_list;
+
+ALTER TABLE stake_orders DROP CONSTRAINT stake_orders_pkey;
+ALTER TABLE stake_orders ADD CONSTRAINT stake_orders_pkey PRIMARY KEY (id);
+ALTER TABLE stake_orders ADD CONSTRAINT stake_orders_trs_id_fk FOREIGN KEY (id) REFERENCES trs (id) ON DELETE CASCADE;
+CREATE INDEX on stake_orders ("stakeId");
+
+COMMIT;
