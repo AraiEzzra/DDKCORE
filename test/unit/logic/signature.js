57ec757cff50e2a11fe007c141bd2c21ea8a4a71
Werdffelynir <werdffelynir@gmail.com>
2019-01-10 20:15:47 +0200

    logic/signature

--- test/unit/logic/signature.js
+++ test/unit/logic/signature.js
@@ -16,34 +16,19 @@ const Logger = require('../../../logger.js');
 const logger = (new Logger()).logger;
 
 const SignatureLogic = require('../../../logic/signature.js');
-const signatureLogic = new SignatureLogic (schema, logger);
 
-;
-
-// let diff = require('../../../helpers/diff.js');
-//
-// let modulesLoader = require('../../common/initModule').modulesLoader;
-// let TransactionLogic = require('../../../logic/transaction.js');
-// let Vote = require('../../../logic/vote.js');
-// let Transfer = require('../../../logic/transfer.js');
-// let Delegate = require('../../../logic/delegate.js');
-// let Rounds = require('../../../modules/rounds.js');
-// let AccountLogic = require('../../../logic/account.js');
-// let FrozenLogic = require('../../../logic/frozen.js');
-// let AccountModule = require('../../../modules/accounts.js');
-// let DelegateModule = require('../../../modules/delegates.js');
-//
-// let validPassword = 'ancient eyebrow pattern twin obtain absent speak bless virus tackle hill equip';
-// let validKeypair = ed.makeKeypair(crypto.createHash('sha256').update(validPassword, 'utf8').digest());
 
 
 
 describe('Logic/Signature', function () {
 
+  let validPassword = 'ancient eyebrow pattern twin obtain absent speak bless virus tackle hill equip';
+  let validKeypair;
   let signatureLogic;
   let bytes;
 
   before(function (done) {
+    validKeypair = ed.makeKeypair(crypto.createHash('sha256').update(validPassword, 'utf8').digest());
     signatureLogic = new SignatureLogic (schema, logger);
     done()
   });
@@ -56,17 +41,22 @@ describe('Logic/Signature', function () {
     it('should return a buffer bytes', function (done) {
 
       bytes = signatureLogic.getBytes({
-        "type": 10,
+        "type": transactionTypes.SIGNATURE,
         "amount": "100000000000",
         "fee": 0,
-        "timestamp": 0,
         "recipientId": "DDK933553974927686133",
         "senderId": "DDK4995063339468361088",
         "senderPublicKey": "f4ae589b02f97e9ab5bce61cf187bcc96cfb3fdf9a11333703a682b7d47c8dc2",
         "signature": "b306025c137902209f6d68ccc97dcaa09c6636fae7869c6192bb76add6b8bbb2ee317e97be7dc8eab72c0ba567fdb795f93edec3d9eabc2c0ba1029adf94b106",
         "id": "f57bcef5a2af67d2ecea7e2c5833b1035c87680a625bb712963a17775e2de851",
         "stakedAmount": 0,
-        "trsName": "SEND"
+        "trsName": "SIGNATURE",
+        "timestamp": 95365881,
+        "asset": {
+          signature: {
+            publicKey: "f4ae589b02f97e9ab5bce61cf187bcc96cfb3fdf9a11333703a682b7d47c8dc2"
+          }
+        }
       });
 
       console.log(' >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> bytes', bytes);
