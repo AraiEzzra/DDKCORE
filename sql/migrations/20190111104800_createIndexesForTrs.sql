5b6bebc26b0ded925d56dc22076bdfc5bd19fbd0
v.voronov <v.voronov@sk-consulting.com.ua>
2019-01-11 11:00:41 +0200

    add scriptes for create indexes

---
+++ sql/migrations/20190111104800_createIndexesForTrs.sql
@@ -0,0 +1,14 @@
+BEGIN;
+
+DROP INDEX IF EXISTS trs_upper_recipient_id RESTRICT;
+CREATE INDEX trs_upper_recipient_id
+  ON trs ("recipientId");
+DROP INDEX IF EXISTS trs_upper_sender_id RESTRICT;
+CREATE INDEX trs_upper_sender_id
+  ON trs ("senderId");
+
+DROP INDEX IF EXISTS trs_encode_sender_rcpt RESTRICT;
+CREATE INDEX trs_encode_sender_rcpt
+  ON public.trs (encode("senderPublicKey", 'hex' :: TEXT), "recipientId");
+
+COMMIT;
