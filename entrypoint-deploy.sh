fbbf64ee18239e5843c2a46b58e25a30a8250b08
Oleksii Fisenko <a.fisenko@sk-consulting.com.ua>
2019-01-11 19:17:19 +0200

    add webpack. add prod dockerfile

---
+++ entrypoint-deploy.sh
@@ -0,0 +1,8 @@
+echo $(node -v)
+# needs defined DB_HOST, ELASTICSEARCH_HOST and REDIS_HOST in environment
+# and wait-port npm package installed
+
+wait-port "$DB_HOST:${DB_PORT:-5432}" &&
+wait-port "$ELASTICSEARCH_HOST" && \
+wait-port "$REDIS_HOST:${REDIS_PORT:-6379}" && \
+npm start
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const DIR = path.resolve(__dirname);
+const nodeExternals = require('webpack-node-externals');
+
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: path.join(DIR, 'src', 'app.js'),
+    context: path.resolve(DIR, "src"),
+    target: "node",
+    externals: [nodeExternals()],
+    node: {
+        __dirname: false,
+        __filename: false,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+            {
+                test: /\.(sql|ttf|gif|png|ico)$/,
+                use: 'file-loader',
+            },
+            {
+                test: /\.(html)$/,
+                use: 'html-loader',
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.js', '.json'],
+        alias: {
+            src: path.resolve(__dirname, 'src'),
+        }
+    },
+    output: {
+        filename: "app.js",
+        path: path.join(DIR, 'dist'),
+        publicPath: '/',
+    },
+    plugins: [
+        new CopyWebpackPlugin([
+            {
+                from: './build',
+                to: './'
+            }
+        ]),
+    ],
+};
