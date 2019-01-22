f2833389188a62e21bd752772db4b61a6b996b0e
David Askarov <david.askarovit@gmail.com>
2019-01-22 19:12:11 +0200

    added abstarct methods

--- api/contoller/delegate.ts
+++ api/contoller/delegate.ts
@@ -4,6 +4,7 @@ import { DelegateModel } from 'shared/model/delegate';
 
 interface IDelegatesArray {
     delegates: DelegateModel[];
+    totalCount?: number;
 }
 
 interface IDelegateCount {
@@ -16,11 +17,19 @@ interface IDelegateSearchRequest {
 }
 interface IPublicKey {
     publicKey: string;
+    username?: string;
+}
+
+interface IGetDelegates {
+    orderBy: string;
+    limit: number;
+    offset: number;
 }
 
 export class DelegateController {
     private service = new DelegateService();
 
+    @GET('/count')
     public async count(req: Request, res: Response) {
         try {
             const count: number = await this.service.count();
@@ -30,6 +39,7 @@ export class DelegateController {
         }
     }
 
+    @GET('/search')
     public async search(req: Request, res: Response) {
         try {
             const { q, limit } = <IDelegateSearchRequest> req.query;
@@ -40,14 +50,36 @@ export class DelegateController {
         }
     }
 
-
     /**
      * Need to Account Model
      * @param req
      * @param res
      */
+    @GET('/voters')
     public async getVoters(req: Request, res: Response) {
-        const { publicKey } = <IPublicKey> req.query;
-        const result: null = await this.service.getVoters(publicKey);
+        try {
+            const { publicKey } = <IPublicKey> req.query;
+            const result: null = await this.service.getVoters(publicKey);
+        } catch (error) {
+        }
+    }
+
+    @GET('/')
+    public async getDelegates(req: Request, res: Response) {
+        try {
+            const { orderBy, limit, offset } = <IGetDelegates> req.query;
+            const result: IDelegatesArray = await this.service.getDelegates(orderBy, limit, offset);
+        } catch (error) {
+        }
+    }
+
+    @GET('/get')
+    public async getDelegate(req: Request, res: Response) {
+        try {
+            const { publicKey, username } = <IPublicKey> req.query;
+            const delegate: DelegateModel = await this.service.getDelegate(publicKey, username);
+            const result = { delegate };
+        } catch (error) {
+        }
     }
 }
