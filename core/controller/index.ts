81f4d7297fc3d13be4d3bb5f163c833e5add4ff7
David Askarov <david.askarovit@gmail.com>
2019-01-28 15:36:16 +0200

    added validate middleware

--- core/controller/index.ts
+++ core/controller/index.ts
@@ -1,5 +1,5 @@
-import { PeerController } from './peer';
-
-export {
-    PeerController
-};
+// import { PeerController } from './peer';
+//
+// export {
+//     PeerController
+// };
--- core/util/decorator.ts
+++ core/util/decorator.ts
@@ -1,51 +1,129 @@
-import { Application } from 'express';
+import { Application, Request, Response as Resp, NextFunction } from 'express';
+import Response from 'shared/model/response';
+import { validate } from './validate';
+
+let targets = new Set();
+
+interface IPropertyRoute {
+    method: string;
+    path: string;
+    func: Function;
+    validate?: Object;
+    propertyKey?: string;
+}
+
 const MethodEnum = {
     GET: 'get',
     POST: 'post',
-    PUT: 'put'
+    PUT: 'put',
+    USE: 'use'
 };
-let targets = new Set();
 
 export const setRoute = (app: Application) => {
     if (app) {
         targets.forEach(item => {
-            item.ctrl.forEach(route => {
+            item.ctrl.forEach((route: IPropertyRoute) => {
+                if (route.validate) {
+                    /**
+                     * Add middleware for validate request
+                     */
+                    app[MethodEnum.USE](item.path + route.path, (req: Request, res: Resp, next: NextFunction) => {
+                        if (!validate(route.validate, route.propertyKey)) {
+                            return new Response({
+                                errors: ['Bad request.']
+                            });
+                        }
+                        next();
+                    });
+                }
+                /**
+                 * Add route
+                 */
                 app[route.method](item.path + route.path, route.func);
             });
         });
     }
 };
 
+/**
+ *
+ * @param path
+ * @constructor
+ */
 export const Controller = (path: string) => {
     return (controller: Function) => {
         if (controller.prototype.routes) {
-            targets.add({
-                path,
-                ctrl: controller.prototype.routes
-            });
+            const body = {
+                path,                              // path controller
+                ctrl: controller.prototype.routes, // path to controller methods
+            };
+            targets.add(body);
         }
     };
 };
-
+/**
+ *
+ * @param path
+ * @constructor
+ */
 export const GET = (path: string) => {
     return (target: any, propertyKey: string, descriptor: PropertyDescriptor) => {
-        addEndpointPath(MethodEnum.GET, path, target, descriptor);
+        addEndpointPath(MethodEnum.GET, path, target, descriptor, propertyKey);
     };
 };
 
+/**
+ *
+ * @param path
+ * @constructor
+ */
 export const POST = (path: string) => {
     return (target: any, propertyKey: string, descriptor: PropertyDescriptor) => {
-        addEndpointPath(MethodEnum.POST, path, target, descriptor);
+        addEndpointPath(MethodEnum.POST, path, target, descriptor, propertyKey);
     };
 };
 
+/**
+ *
+ * @param path
+ * @constructor
+ */
 export const PUT = (path: string) => {
     return (target: any, propertyKey: string, descriptor: PropertyDescriptor) => {
-        addEndpointPath(MethodEnum.PUT, path, target, descriptor);
+        addEndpointPath(MethodEnum.PUT, path, target, descriptor, propertyKey);
     };
 };
 
-const addEndpointPath = (method: string, path: string,  target: any, descriptor: PropertyDescriptor) => {
+/**
+ * Decorator for Validate request, that need to be after decorator GET, PUT or POST
+ * @param data
+ * @constructor
+ */
+const Validate = (data) => {
+    return ( target: any, propertyName: string, descriptor: PropertyDescriptor) => {
+      if (data) {
+          Object.defineProperty(target, 'validate', {
+              value: data,
+              configurable: false,
+              writable: false,
+              enumerable: false
+          });
+      }
+    };
+};
+
+/**
+ * Add route to list
+ * @param method
+ * @param path
+ * @param target
+ * @param descriptor
+ */
+const addEndpointPath = (method: string,
+                         path: string,
+                         target: any,
+                         descriptor: PropertyDescriptor,
+                         propertyKey: string) => {
     if (!target.hasOwnProperty('routes')) {
         Object.defineProperty(target, 'routes', {
             value: [],
@@ -54,12 +132,16 @@ const addEndpointPath = (method: string, path: string,  target: any, descriptor:
             enumerable: false
         });
     }
-    target.routes.push({
+    const propertyRoute: IPropertyRoute = {
         method,
         path,
-        'func': descriptor.value
-    });
+        func: descriptor.value,
+        validate: target.validate,
+        propertyKey
+    };
+    target.routes.push(propertyRoute);
 };
+
 
 // TODO Need to Remove
 // @Controller('/test1')
@@ -67,6 +149,7 @@ const addEndpointPath = (method: string, path: string,  target: any, descriptor:
 //     constructor() {}
 //
 //     @GET('/')
+//     @Validate({})
 //     test1(req, res) {
 //         res.json({
 //             message: 'Class 1. Path "/" \n'
@@ -79,9 +162,12 @@ const addEndpointPath = (method: string, path: string,  target: any, descriptor:
 //     constructor() {}
 //
 //     @GET('/')
+//     @Validate({ body: 'test validate' })
 //     test(req, res) {
 //         res.json({
 //             message: 'Class 2. Path "/test"'
 //         });
 //     }
 // }
+
+
