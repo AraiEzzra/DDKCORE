d116a8e0bcad0685857070f070a1ef3fe369a27b
Werdffelynir <werdffelynir@gmail.com>
2019-02-05 19:26:35 +0200

    fixed rpc method create transaction

--- src/api/rpc/methods/createTransaction.js
+++ src/api/rpc/methods/createTransaction.js
@@ -3,14 +3,6 @@ const ReservedError = require('../errors');
 const crypto = require('crypto');
 const transactionTypes = require('../../../helpers/transactionTypes.js');
 
-const transactionRequired = [
-    'type',
-    'amount',
-    'senderAddress',
-    'requesterAddress',
-];
-
-const transactionDefault = {};
 
 module.exports = createServerRPCMethod(
     'CREATE_TRANSACTION',
@@ -21,42 +13,51 @@ module.exports = createServerRPCMethod(
      * @param {object} scope - Application instance
      */
     (wss, params, scope) => new Promise((resolve) => {
-        const accepted = transactionRequired.every(key => params[key]);
 
-        if (Object.keys(transactionTypes).indexOf(params.type) > -1 && accepted) {
-            scope.modules.accounts.shared.getAccount({ body: { address: params.senderAddress } }, (error, result) => {
-                if (error || (result && result.account)) {
-                    const sender = result.account;
-                    const hash = crypto.createHash('sha256').update(sender.publicKey, 'utf8').digest();
-                    const keypair = scope.ed.makeKeypair(hash);
-                    let trs;
-
-                    scope.modules.accounts.shared.getAccount({ body: { address: params.requesterAddress } },
-                        async (requesterError, requesterResult) => {
-                            if (requesterResult) {
-                                try {
-                                    const requester = requesterResult.account;
-                                    trs = await scope.logic.transaction.create({
-                                        type: transactionTypes[params.type],
-                                        amount: params.amount,
-                                        sender,
-                                        requester,
-                                        keypair,
-                                    });
-
-                                    resolve({ transaction: trs });
-                                } catch (err) {
-                                    resolve({ error: err });
-                                }
-                            } else {
-                                resolve({ error: requesterError });
-                            }
-                        });
-                } else {
-                    resolve({ error });
-                }
-            });
-        } else {
-            resolve({ error: ReservedError.ServerErrorInvalidMethodParameters });
+        if (!transactionTypes[params.type]) {
+            resolve({error: `Unknown transaction type [${params.type}]`});
+            return;
         }
-    }));
+
+        scope.modules.accounts.shared.getAccount({ body: { address: params.senderAddress } }, async (error, senderAccount) => {
+            if (error || !senderAccount.account || !senderAccount.account.publicKey) {
+                resolve({error: error || `Incorrect sender account ${params.senderAddress}`});
+                return;
+            }
+
+            const hash = crypto.createHash('sha256').update(senderAccount.account.publicKey, 'utf8').digest();
+            const keypair = scope.ed.makeKeypair(hash);
+
+            const createTransaction = async (sender, requester) => {
+                let transaction;
+                let error;
+                try {
+                    transaction = await scope.logic.transaction.create({
+                        type: transactionTypes[params.type],
+                        amount: params.amount,
+                        sender,
+                        requester,
+                        keypair,
+                    });
+                } catch (err) {
+                    error = err.message;
+                }
+                resolve(error ? {error} : {transaction} );
+            };
+
+            if (params.requesterAddress) {
+                scope.modules.accounts.shared.getAccount({ body: { address: params.requesterAddress } },
+                    async (error, requesterAccount) => {
+                        if (error) {
+                            resolve({error});
+                            return;
+                        }
+                        await createTransaction(senderAccount.account, requesterAccount.account);
+                    });
+            } else {
+                await createTransaction(senderAccount.account);
+            }
+        });
+
+    })
+);
