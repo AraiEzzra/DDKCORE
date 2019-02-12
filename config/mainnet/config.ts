ec130a59288bbfa1b026446e1010a84adb2b4916
Sergey <pal4ik.s@gmail.com>
2019-02-12 08:10:51 +0200

    config and constants implementation

---
+++ config/mainnet/config.ts
@@ -0,0 +1,150 @@
+const env = process.env;
+
+export default {
+    port: parseInt(env.PORT, 10) || 7000,
+    address: env.ADDRESS || '0.0.0.0',
+    version: '0.9.9a',
+    minVersion: '>=0.9.0',
+    fileLogLevel: 'info',
+    logFileName: 'logs/ddk.log',
+    consoleLogLevel: 'none',
+    trustProxy: false,
+    topAccounts: false,
+    cacheEnabled: true,
+    db: {
+        host: env.DB_HOST || '0.0.0.0',
+        port: parseInt(env.DB_PORT, 10) || 5432,
+        database: env.DB_NAME || 'ddk_mainnet_database',
+        password: env.DB_PASSWORD,
+        user: env.DB_USER,
+        poolSize: 95,
+        poolIdleTimeout: 30000,
+        reapIntervalMillis: 1000,
+        logEvents: [
+            'error'
+        ]
+    },
+    redis: {
+        host: env.REDIS_HOST || '0.0.0.0',
+        port: parseInt(env.REDIS_PORT, 10) || 6379,
+        password: env.REDIS_PASSWORD,
+        db: 0
+    },
+    api: {
+        enabled: true,
+        access: {
+            public: true,
+            whiteList: []
+        },
+        options: {
+            limits: {
+                max: 0,
+                delayMs: 0,
+                delayAfter: 0,
+                windowMs: 60000
+            }
+        }
+    },
+    peers: {
+        enabled: true,
+        list: (env.PEERS || '').split(',').map(peer => peer.split(':')).map(([ip, port]) => ({ ip, port })),
+        access: {
+            blackList: (env.PEERS_BLACKLIST || '').split(','),
+        },
+        options: {
+            limits: {
+                max: 0,
+                delayMs: 0,
+                delayAfter: 0,
+                windowMs: 60000
+            },
+            timeout: 5000
+        }
+    },
+    broadcasts: {
+        broadcastInterval: 5000,
+        broadcastLimit: 25,
+        parallelLimit: 20,
+        releaseLimit: 25,
+        relayLimit: 3
+    },
+    transactions: {
+        maxTxsPerQueue: 1000
+    },
+    forging: {
+        force: true,
+        minBroadhashConsensus: parseInt(env.MIN_CONSENSUS, 10) || 51,
+        secret: env.FORGE_SECRET,
+        access: {
+            whiteList: []
+        }
+    },
+    loading: {
+        verifyOnLoading: env.VERIFY_ON_LOADING === 'TRUE',
+        loadPerIteration: 5000
+    },
+    ssl: {
+        enabled: false,
+        options: {
+            port: 443,
+            address: env.ADDRESS || '0.0.0.0',
+            key: env.SSL_KEY,
+            cert: env.SSL_CERT
+        }
+    },
+    dapp: {
+        masterrequired: true,
+        autoexec: [],
+        masterpassword: env.DAPP_MASTERPASSWORD
+    },
+    initialPrimined: {
+        total: 360000000000000
+    },
+    ddkSupply: {
+        totalSupply: 4500000000000000
+    },
+    session: {
+        secret: env.SESSION_SECRET
+    },
+    sender: {},
+    nethash: env.NETHASH,
+    elasticsearchHost: env.ELASTICSEARCH_HOST || '0.0.0.0:9200',
+    elasticsearch: {
+        disableJobs: env.DISABLE_ELASTICSEARCH_JOBS === 'TRUE',
+    },
+    jwt: {
+        secret: env.JWT_SECRET,
+        tokenLife: 300
+    },
+    mailFrom: env.MAIL_FROM,
+    hashSecret: env.HASH_SECRET,
+    users: [
+        {
+            keys: env.DSTAKEREWARD
+        },
+        {
+            keys: env.DCONTRIBUTOR
+        },
+        {
+            keys: env.DADVISOR
+        },
+        {
+            keys: env.DTEAM
+        },
+        {
+            keys: env.DFOUNDER
+        },
+        {
+            keys: env.DPENDINGGB
+        },
+        {
+            keys: env.DAIRDROP
+        },
+        {
+            keys: env.DRESERVEDEX
+        },
+        {
+            keys: env.DPREORDERDNC
+        }
+    ]
+};
---
+++ config/schema/config.ts
@@ -0,0 +1,292 @@
+export default {
+    config: {
+        id: 'appCon',
+        type: 'object',
+        properties: {
+            address: {
+                type: 'string',
+                format: 'ip'
+            },
+            version: {
+                type: 'string',
+                format: 'version',
+                minLength: 5,
+                maxLength: 12
+            },
+            minVersion: {
+                type: 'string'
+            },
+            fileLogLevel: {
+                type: 'string'
+            },
+            logFileName: {
+                type: 'string'
+            },
+            consoleLogLevel: {
+                type: 'string'
+            },
+            trustProxy: {
+                type: 'boolean'
+            },
+            topAccounts: {
+                type: 'boolean'
+            },
+            cacheEnabled: {
+                type: 'boolean'
+            },
+            db: {
+                type: 'object',
+                properties: {
+                    host: {
+                        type: 'string',
+                    },
+                    port: {
+                        type: 'integer',
+                        minimum: 1,
+                        maximum: 65535
+                    },
+                    database: {
+                        type: 'string'
+                    },
+                    user: {
+                        type: 'string'
+                    },
+                    poolSize: {
+                        type: 'integer'
+                    },
+                    poolIdleTimeout: {
+                        type: 'integer'
+                    },
+                    reapIntervalMillis: {
+                        type: 'integer'
+                    },
+                    logEvents: {
+                        type: 'array'
+                    }
+                },
+                required: ['database', 'user', 'poolSize', 'poolIdleTimeout', 'reapIntervalMillis', 'logEvents']
+            },
+            redis: {
+                type: 'object',
+                properties: {
+                    host: {
+                        type: 'string',
+                        format: 'ip',
+                    },
+                    port: {
+                        type: 'integer',
+                        minimum: 1,
+                        maximum: 65535
+                    },
+                    db: {
+                        type: 'integer',
+                        minimum: 0,
+                        maximum: 15
+                    }
+                },
+                required: ['host', 'port', 'db']
+            },
+            api: {
+                type: 'object',
+                properties: {
+                    enabled: {
+                        type: 'boolean'
+                    },
+                    access: {
+                        type: 'object',
+                        properties: {
+                            public: {
+                                type: 'boolean'
+                            },
+                            whiteList: {
+                                type: 'array'
+                            }
+                        },
+                        required: ['public', 'whiteList']
+                    },
+                    options: {
+                        type: 'object',
+                        properties: {
+                            limits: {
+                                type: 'object',
+                                properties: {
+                                    max: {
+                                        type: 'integer'
+                                    },
+                                    delayMs: {
+                                        type: 'integer'
+                                    },
+                                    delayAfter: {
+                                        type: 'integer'
+                                    },
+                                    windowMs: {
+                                        type: 'integer'
+                                    }
+                                },
+                                required: ['max', 'delayMs', 'delayAfter', 'windowMs']
+                            }
+                        },
+                        required: ['limits']
+                    }
+                },
+                required: ['enabled', 'access', 'options']
+            },
+            peers: {
+                type: 'object',
+                properties: {
+                    enabled: {
+                        type: 'boolean'
+                    },
+                    list: {
+                        type: 'array'
+                    },
+                    access: {
+                        type: 'object',
+                        properties: {
+                            blackList: {
+                                type: 'array'
+                            }
+                        },
+                        required: ['blackList']
+                    },
+                    options: {
+                        properties: {
+                            limits: {
+                                type: 'object',
+                                properties: {
+                                    max: {
+                                        type: 'integer'
+                                    },
+                                    delayMs: {
+                                        type: 'integer'
+                                    },
+                                    delayAfter: {
+                                        type: 'integer'
+                                    },
+                                    windowMs: {
+                                        type: 'integer'
+                                    }
+                                },
+                                required: ['max', 'delayMs', 'delayAfter', 'windowMs']
+                            },
+                            timeout: {
+                                type: 'integer'
+                            }
+                        },
+                        required: ['limits', 'timeout']
+                    }
+                },
+                required: ['enabled', 'list', 'access', 'options']
+            },
+            broadcasts: {
+                type: 'object',
+                properties: {
+                    broadcastInterval: {
+                        type: 'integer',
+                        minimum: 1000,
+                        maximum: 60000
+                    },
+                    broadcastLimit: {
+                        type: 'integer',
+                        minimum: 1,
+                        maximum: 100
+                    },
+                    parallelLimit: {
+                        type: 'integer',
+                        minimum: 1,
+                        maximum: 100
+                    },
+                    releaseLimit: {
+                        type: 'integer',
+                        minimum: 1,
+                        maximum: 25
+                    },
+                    relayLimit: {
+                        type: 'integer',
+                        minimum: 1,
+                        maximum: 100
+                    }
+                },
+                required: ['broadcastInterval', 'broadcastLimit', 'parallelLimit', 'releaseLimit', 'relayLimit']
+            },
+            transactions: {
+                type: 'object',
+                maxTxsPerQueue: {
+                    type: 'integer',
+                    minimum: 100,
+                    maximum: 5000
+                },
+                required: ['maxTxsPerQueue']
+            },
+            forging: {
+                type: 'object',
+                properties: {
+                    force: {
+                        type: 'boolean'
+                    },
+                    access: {
+                        type: 'object',
+                        properties: {
+                            whiteList: {
+                                type: 'array'
+                            }
+                        },
+                        required: ['whiteList']
+                    }
+                },
+                required: ['force', 'access']
+            },
+            loading: {
+                type: 'object',
+                properties: {
+                    verifyOnLoading: {
+                        type: 'boolean'
+                    },
+                    loadPerIteration: {
+                        type: 'integer',
+                        minimum: 1,
+                        maximum: 5000
+                    }
+                },
+                required: ['verifyOnLoading', 'loadPerIteration']
+            },
+            ssl: {
+                type: 'object',
+                properties: {
+                    enabled: {
+                        type: 'boolean'
+                    },
+                    options: {
+                        type: 'object',
+                        properties: {
+                            port: {
+                                type: 'integer'
+                            },
+                            address: {
+                                type: 'string',
+                                format: 'ip',
+                            }
+                        },
+                        required: ['port', 'address']
+                    }
+                },
+                required: ['enabled', 'options']
+            },
+            dapp: {
+                type: 'object',
+                properties: {
+                    masterrequired: {
+                        type: 'boolean'
+                    },
+                    autoexec: {
+                        type: 'array'
+                    }
+                },
+                required: ['masterrequired', 'autoexec']
+            }
+        },
+        required: ['address', 'version', 'minVersion', 'fileLogLevel', 'logFileName', 'consoleLogLevel', 
+            'trustProxy', 'topAccounts', 'db', 'api', 'peers', 'broadcasts', 'transactions', 'forging', 
+            'loading', 'ssl', 'dapp', 'cacheEnabled', 'redis']
+    }
+};
