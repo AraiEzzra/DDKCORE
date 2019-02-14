ca2a210650afc4e24e74c715dc006c4ca0888853
Oleksii Fisenko <a.fisenko@sk-consulting.com.ua>
2019-02-14 17:56:07 +0200

    new models. change database structure

--- shared/model/block.ts
+++ shared/model/block.ts
@@ -1,60 +1,18 @@
+import { Timestamp } from 'shared/model/account';
 import { Transaction } from 'shared/model/transaction';
 
-interface IBlockFields {
-    id?: string;
-    rowId?: number;
-    version?: number;
-    timestamp?: number;
-    height?: number;
-    previousBlock?: string;
-    numberOfTransactions?: number;
-    totalAmount?: bigint | number;
-    totalFee?: bigint | number;
-    reward?: number;
-    payloadLength?: number;
-    payloadHash?: string;
-    generatorPublicKey?: string;
-    blockSignature?: string;
-    generationSignature?: string;
-    totalForged?: bigint | number;
-    generatorId?: string;
-    confirmations?: number;
-    username?: string;
-    transactions? : Transaction<object>[];
-}
-
 export class Block {
-    id?: string;
-    rowId?: number;
-    version?: number;
-    timestamp?: number;
-    height?: number;
-    previousBlock?: string;
-    numberOfTransactions?: number;
-    totalAmount?: bigint;
-    totalFee?: bigint;
-    reward?: number;
-    payloadLength?: number;
-    payloadHash?: string;
-    generatorPublicKey?: string;
-    blockSignature?: string;
-    generationSignature?: string;
-    totalForged?: bigint;
-    generatorId?: string;
-    confirmations?: number;
-    username?: string;
-    transactions? : Transaction<object>[];
+    id: string;
+    version: number;
+    createdAt: Timestamp;
+    height: number;
+    previousBlockId: string | null;
+    transactionCount: number;
+    amount: number;
+    fee: number;
+    payloadHash: string;
+    generatorPublicKey: string;
+    signature: string;
+    transactions: Array<Transaction<object>>;
 
-    // todo: resolve = weak for passing untyped object
-    constructor(data: IBlockFields) {
-        for (const key in data) {
-            if (data[key] || data[key] === 0) {
-                if (key === 'totalAmount' || key === 'totalFee' || key === 'totalForged') {
-                    this[key] = BigInt(data[key]);
-                } else {
-                    this[key] = data[key];
-                }
-            }
-        }
-    }
 }
