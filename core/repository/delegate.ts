79df1db26a985caab5f182766ed3c211c32f81f2
Sergey <pal4ik.s@gmail.com>
2019-02-20 18:57:51 +0200

    mock delegates

--- core/repository/delegate.ts
+++ core/repository/delegate.ts
@@ -1,14 +1,23 @@
 import Response from 'shared/model/response';
 import { Account } from 'shared/model/account';
 import Config from 'shared/util/config';
-import { IDelegate } from 'shared/model/delegate';
+import { Delegate } from 'shared/model/delegate';
 
 const constants = Config.constants;
 
 class DelegateRepository {
-    private memoryDelegates: Array<IDelegate> = [];
-    
-    public addDelegate(delegate: IDelegate) {
+    private memoryDelegates: Array<Delegate> = [];
+
+    public addDelegate(account: Account, params: {username?: string, url?: string} = {}) {
+        const delegate: Delegate = new Delegate({
+            username: params.username || '',
+            url: params.url || '',
+            missedBlocks: 0,
+            forgedBlocks: 0,
+            account: account,
+            votes: 0
+        });
+        
         this.memoryDelegates.push(delegate);
         this.memoryDelegates.sort((a, b) => {
             if (a.votes > b.votes) {
@@ -19,17 +28,19 @@ class DelegateRepository {
             }
             return 0;
         });
+        
+        return delegate;
     }
 
     /**
      * @implements constants.activeDelegates
      * @return Array<Account>
      */
-    public getActiveDelegates(): Response<Array<IDelegate>> {
+    public getActiveDelegates(): Response<Array<Delegate>> {
         return new Response({data: this.memoryDelegates.slice(0, constants.activeDelegates)});
     }
 
-    public updateDelegate(delegate: IDelegate) {
+    public updateDelegate(delegate: Delegate) {
         for (let i = 0; i < this.memoryDelegates.length; i++) {
             if ( delegate.account.address === this.memoryDelegates[i].account.address) {
                 this.memoryDelegates[i] = delegate;
