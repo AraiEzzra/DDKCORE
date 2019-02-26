7946180da13ea456d5fc42f375d1df23dfe4ed11
David Askarov <david.askarovit@gmail.com>
2019-02-26 14:46:58 +0200

    added getBytes

--- core/service/transaction/stake.ts
+++ core/service/transaction/stake.ts
@@ -1,12 +1,14 @@
 import { ITransactionService } from '../transaction';
 import {IAsset, IAirdropAsset} from 'shared/model/transaction';
-import { IAssetStake, Transaction } from 'shared/model/transaction';
+import { IAssetStake, Transaction, TransactionType } from 'shared/model/transaction';
 import ResponseEntity from 'shared/model/response';
 import {Account} from 'shared/model/account';
 import AccountRepo from 'core/repository/account';
+import BlockRepo from 'core/repository/block';
 import { TOTAL_PERCENTAGE } from 'core/util/const';
 import config from 'shared/util/config';
 import BUFFER from 'core/util/buffer';
+
 import {
     calculateTotalRewardAndUnstake,
     getAirdropReward,
@@ -14,13 +16,14 @@ import {
     applyFrozeOrdersRewardAndUnstake,
     undoFrozeOrdersRewardAndUnstake
 } from 'core/util/reward';
+import {Block} from 'shared/model/block';
 
 class TransactionStakeService implements ITransactionService<IAssetStake> {
 
     async create(trs: Transaction<IAssetStake>, data?: IAssetStake ): Promise<IAssetStake> {
         const sender: Account = AccountRepo.getByAddress(trs.senderAddress);
-        const totals: { reward: number, unstake: number} = await calculateTotalRewardAndUnstake(sender, isDownVote);
-        const airdropReward: IAirdropAsset = await getAirdropReward(sender, totals.reward, trs.type);
+        const totals: { reward: number } = await calculateTotalRewardAndUnstake(sender, false);
+        const airdropReward: IAirdropAsset = await getAirdropReward(sender, totals.reward, TransactionType.STAKE);
 
         const asset: IAssetStake = {
             stakeOrder: {
@@ -34,8 +37,41 @@ class TransactionStakeService implements ITransactionService<IAssetStake> {
         return asset;
     }
 
-    getBytes(asset: IAssetStake): Uint8Array {
-        return Buffer.from([]);
+    getBytes(trs: Transaction<IAssetStake>): Buffer {
+        let offset = 0;
+        const buff = Buffer.alloc(
+            BUFFER.LENGTH.INT64 +  // asset.stakeOrder.stakedAmount
+            // BUFFER.LENGTH.UINT32 + // asset.stakeOrder.nextVoteMilestone
+            BUFFER.LENGTH.UINT32 + // asset.stakeOrders.startTime
+            BUFFER.LENGTH.BYTE +   // asset.airdropReward.withAirdropReward
+            BUFFER.LENGTH.INT64    // asset.airdropReward.totalReward
+        );
+        offset = BUFFER.writeUInt64LE(buff, trs.asset.stakeOrder.stakedAmount || 0, offset);
+        /**
+         * TODO Should be async?
+         */
+        // const block: ResponseEntity<Block> = BlockRepo.loadFullBlockById(trs.blockId);
+
+        // if (block.data.height <= config.constants.MASTER_NODE_MIGRATED_BLOCK) {
+        //     buff.writeInt32LE(trs.asset.stakeOrder.nextVoteMilestone, offset);
+        // }
+
+        offset += BUFFER.LENGTH.UINT32;
+        buff.writeInt32LE(trs.asset.stakeOrder.startTime, offset);
+        offset += BUFFER.LENGTH.UINT32;
+        buff.writeInt8(trs.asset.airdropReward.withAirdropReward ? 1 : 0, offset);
+        offset += BUFFER.LENGTH.BYTE;
+        BUFFER.writeUInt64LE(buff, trs.asset.airdropReward.totalReward || 0, offset);
+
+        const sponsorsBuffer = Buffer.alloc(BUFFER.LENGTH.INT64 + BUFFER.LENGTH.INT64);
+        offset = 0;
+        if (trs.asset.airdropReward.sponsors && Object.keys(trs.asset.airdropReward.sponsors).length > 0) {
+            const address = Object.keys(trs.asset.airdropReward.sponsors)[0];
+            offset = BUFFER.writeUInt64LE(sponsorsBuffer, parseInt(address.slice(3), 10), offset);
+            BUFFER.writeUInt64LE(sponsorsBuffer, trs.asset.airdropReward.sponsors[address] || 0, offset);
+        }
+
+        return Buffer.concat([buff, sponsorsBuffer]);
     }
 
     verifyUnconfirmed(trs: Transaction<IAsset>): ResponseEntity<void> {
