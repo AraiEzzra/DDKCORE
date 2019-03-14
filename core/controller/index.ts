fa79726f1e1f46a6403c06ab2358f9a4022814e3
Dmytro Mehed <d.mehed@sk-consulting.com.ua>
2019-03-14 09:13:31 +0200

    Create one queue for receiveBlock and generate block https://trello.com/c/8qpoL6Bt/140-create-one-queue-for-receiveblock-and-generate-block

--- core/controller/index.ts
+++ core/controller/index.ts
@@ -3,8 +3,10 @@ import DelegateController from './delegate';
 import RoundController from './round';
 import SyncController from './sync';
 import TransactionController from './transaction';
+import { filter } from 'rxjs/operators';
 
 import { subjectOn, subjectRpc } from 'shared/util/bus';
+import { logger } from 'shared/util/logger';
 
 export const initControllers = () => {
     const controllers = [
@@ -14,6 +16,19 @@ export const initControllers = () => {
         DelegateController,
         SyncController
     ];
+
+    console.log(JSON.stringify(BlockController.eventsMAIN));
+    subjectOn
+    .pipe(
+        filter((elem: { data, topicName }) =>
+            ['BLOCK_GENERATE', 'BLOCK_RECEIVE'].indexOf(elem.topicName) !== -1
+        )
+    )
+    .subscribe(async ({ data, topicName }) => {
+        logger.debug(`TASK MAIN ${topicName} start`);
+        await BlockController.eventsMAIN[topicName].apply(BlockController, [data]);
+        logger.debug(`TASK MAIN ${topicName} finish`);
+    });
 
     controllers.forEach((controller) => {
         if (controller.eventsON && controller.eventsON.length) {
