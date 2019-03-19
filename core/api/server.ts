15ee2c171a8287b7d01b43e53dc0c0759c8e8547
Oleksii Fisenko <a.fisenko@sk-consulting.com.ua>
2019-03-19 15:07:56 +0200

    add properties to socket config

--- core/api/server.ts
+++ core/api/server.ts
@@ -1,4 +1,4 @@
 import { CoreRPCSocketServer } from 'core/api/socket';
-import { API_SOCKET_SERVER_CONFIG, CORE_RPC_PORT } from 'shared/config/socket';
+import { CORE_RPC_PORT, CORE_SOCKET_SERVER_CONFIG } from 'shared/config/socket';
 
-export const socketRPCServer = new CoreRPCSocketServer(CORE_RPC_PORT, API_SOCKET_SERVER_CONFIG);
+export const socketRPCServer = new CoreRPCSocketServer(CORE_RPC_PORT, CORE_SOCKET_SERVER_CONFIG);
