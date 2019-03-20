9ca7594da5cfe2de849aed454ec1d43c4fd630a4
Aleksandr Shevchenko <axelsheva94@gmail.com>
2019-03-20 12:33:40 +0200

    Fix round calculation

--- core/service/round.ts
+++ core/service/round.ts
@@ -19,7 +19,7 @@ import { compose } from 'core/util/common';
 import RoundPGRepository from 'core/repository/round/pg';
 import { Block } from 'shared/model/block';
 import { ActionTypes } from 'core/util/actionTypes';
-import { calculateRoundByTimestamp } from 'core/util/round';
+import { calculateRoundFirstSlotByTimestamp } from 'core/util/round';
 
 const MAX_LATENESS_FORGE_TIME = 500;
 const constants = Config.constants;
@@ -120,7 +120,7 @@ class RoundService implements IRoundService {
     }
 
     public generatorPublicKeyToSlot(sortedHashList: Array<IHashList>, timestamp: number): Slots {
-        let firstSlot = calculateRoundByTimestamp(timestamp);
+        let firstSlot = calculateRoundFirstSlotByTimestamp(timestamp);
         return sortedHashList.reduce(
             (acc: Slots = {}, item: IHashList, i) => {
                 acc[item.generatorPublicKey] = { slot: firstSlot + i };
--- core/util/round.ts
+++ core/util/round.ts
@@ -1,8 +1,16 @@
 import config from 'shared/util/config';
 import SlotService from 'core/service/slot';
 
+export const calculateRoundFirstSlotByTimestamp = (timestamp: number): number => {
+    return calculateRoundByTimestamp(timestamp) * config.constants.activeDelegates;
+};
+
 export const calculateRoundByTimestamp = (timestamp: number): number => {
     const slot = SlotService.getSlotNumber(timestamp);
 
-    return Math.ceil(slot - 1 / config.constants.activeDelegates);
+    return calculateRound(slot, config.constants.activeDelegates);
+};
+
+export const calculateRound = (slot: number, activeDelegates: number): number => {
+    return Math.ceil(slot / activeDelegates);
 };
