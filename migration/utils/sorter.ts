c6154d5c8c5c26dc9849c1e2174163d0b4516139
Dmytro Mekhed <enshteynn@ukr.net>
2019-04-11 17:05:24 +0300

    add sorter.ts v2

--- migration/utils/sorter.ts
+++ migration/utils/sorter.ts
@@ -1,34 +1,32 @@
-import { SerializedTransaction } from 'shared/model/transaction';
-
 const MAX_TREE_LENGTH = 15;
-type SerializedRegisterTransactions = Array<SerializedTransaction<{ referral: Array<string> }>>;
+type SerializedRegisterTransactions = Array<{ referrals: Array<string>, senderAddress: BigInt }>;
 
-const sortRegisterTrs = (transactions: SerializedRegisterTransactions): SerializedRegisterTransactions => {
+export const sortRegisterTrs = (transactions: SerializedRegisterTransactions): SerializedRegisterTransactions => {
     const migratedAddresses = new Set<string>();
 
     transactions.sort((a, b) => {
-        if (a.asset.referral.length > b.asset.referral.length) {
+        if (a.referrals.length > b.referrals.length) {
             return 1;
         }
-        if (a.asset.referral.length < b.asset.referral.length) {
+        if (a.referrals.length < b.referrals.length) {
             return -1;
         }
         return 0;
     });
 
-    const sortedTransactions = transactions.filter(trs => trs.asset.referral.length < MAX_TREE_LENGTH);
+    const sortedTransactions = transactions.filter(trs => trs.referrals.length < MAX_TREE_LENGTH);
     sortedTransactions.forEach(trs => {
-        migratedAddresses.add(trs.senderAddress);
+        migratedAddresses.add(trs.senderAddress.toString());
     });
 
-    const notSortedTransactions = transactions.filter(trs => trs.asset.referral.length === MAX_TREE_LENGTH);
+    const notSortedTransactions = transactions.filter(trs => trs.referrals.length === MAX_TREE_LENGTH);
     console.log('notSortedTransactions.length', notSortedTransactions.length);
 
     while (notSortedTransactions.length) {
         const trs = notSortedTransactions.shift();
-        if (migratedAddresses.has(trs.asset.referral[0])) {
+        if (migratedAddresses.has(trs.referrals[0])) {
             sortedTransactions.push(trs);
-            migratedAddresses.add(trs.senderAddress);
+            migratedAddresses.add(trs.senderAddress.toString());
         } else {
             notSortedTransactions.push(trs);
         }
