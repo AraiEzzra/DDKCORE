2fadced2fa17f4524dbd31587df716212507ddac
Aleksandr Shevchenko <axelsheva94@gmail.com>
2019-04-18 14:06:27 +0300

    Refactor logs in sync controller

--- core/controller/sync.ts
+++ core/controller/sync.ts
@@ -3,7 +3,6 @@ import { ON } from 'core/util/decorator';
 import { Peer } from 'shared/model/peer';
 import { BaseController } from 'core/controller/baseController';
 import PeerService from 'core/service/peer';
-import RoundService from 'core/service/round';
 import { logger } from 'shared/util/logger';
 import PeerRepository from 'core/repository/peer';
 import { messageON } from 'shared/util/bus';
@@ -25,12 +24,13 @@ type checkCommonBlocksRequest = {
 };
 
 const SYNC_TIMEOUT = 10000;
+const LOG_PREFIX = '[Controller][Sync]';
 
 export class SyncController extends BaseController {
     @ON('REQUEST_COMMON_BLOCKS')
     checkCommonBlocks(action: checkCommonBlocksRequest): void {
         const { data, peer, requestId } = action;
-        logger.debug(`[Controller][Sync][checkCommonBlocks]: ${JSON.stringify(data.block)}`);
+        logger.debug(`${LOG_PREFIX}[checkCommonBlocks]: ${JSON.stringify(data.block)}`);
         SyncService.checkCommonBlocks(data.block, peer, requestId);
     }
 
@@ -39,11 +39,18 @@ export class SyncController extends BaseController {
         if (SyncService.consensus || PeerRepository.peerList().length === 0) {
             System.synchronization = false;
             messageON('WARM_UP_FINISHED');
-            logger.debug(`[Controller][Sync][startSyncBlocks]: Unable to sync`);
+
+            const logMessage = `${LOG_PREFIX}[startSyncBlocks]: Unable to sync`;
+            if (SyncService.consensus) {
+                logger.info(`${logMessage}. Consensus is ${SyncService.getConsensus()}%`);
+            } else if (PeerRepository.peerList().length === 0) {
+                logger.info(`${logMessage}. No peers to sync`);
+            }
+
             return;
         }
         System.synchronization = true;
-        logger.debug(`[Controller][Sync][startSyncBlocks]: start sync with consensus ${SyncService.getConsensus()}%`);
+        logger.debug(`${LOG_PREFIX}[startSyncBlocks]: start sync with consensus ${SyncService.getConsensus()}%`);
 
         // TODO: change sync timeout logic
         let needDelay = false;
@@ -57,13 +64,13 @@ export class SyncController extends BaseController {
 
             const lastBlock = await BlockRepository.getLastBlock();
             if (!lastBlock) {
-                logger.error(`[Controller][Sync][startSyncBlocks] lastBlock is undefined`);
+                logger.error(`${LOG_PREFIX}[startSyncBlocks] lastBlock is undefined`);
                 break;
             }
             const responseCommonBlocks = await SyncService.checkCommonBlock(lastBlock);
             if (!responseCommonBlocks.success) {
                 logger.error(
-                    `[Controller][Sync][startSyncBlocks][responseCommonBlocks]: ` +
+                    `${LOG_PREFIX}[startSyncBlocks][responseCommonBlocks]: ` +
                     responseCommonBlocks.errors.join('. ')
                 );
                 if (responseCommonBlocks.errors.indexOf(REQUEST_TIMEOUT) !== -1) {
@@ -79,7 +86,7 @@ export class SyncController extends BaseController {
             const responseBlocks = await SyncService.requestBlocks(lastBlock, peer);
             if (!responseBlocks.success) {
                 logger.error(
-                    `[Controller][Sync][startSyncBlocks][responseBlocks]: ${responseBlocks.errors.join('. ')}`
+                    `${LOG_PREFIX}[startSyncBlocks][responseBlocks]: ${responseBlocks.errors.join('. ')}`
                 );
                 if (responseCommonBlocks.errors.indexOf(ERROR_NOT_ENOUGH_PEERS) !== -1) {
                     break;
@@ -88,7 +95,7 @@ export class SyncController extends BaseController {
             }
             const loadStatus = await SyncService.loadBlocks(responseBlocks.data);
             if (!loadStatus.success) {
-                logger.error(`[Controller][Sync][startSyncBlocks][loadStatus]: ${loadStatus.errors.join('. ')}`);
+                logger.error(`${LOG_PREFIX}[startSyncBlocks][loadStatus]: ${loadStatus.errors.join('. ')}`);
             } else {
                 needDelay = false;
             }
@@ -96,7 +103,7 @@ export class SyncController extends BaseController {
         System.synchronization = false;
         messageON('WARM_UP_FINISHED');
         EventQueue.process();
-        logger.debug('[Controller][Sync][startSyncBlocks] SYNCHRONIZATION DONE SUCCESS');
+        logger.info(`${LOG_PREFIX}[startSyncBlocks] SYNCHRONIZATION DONE SUCCESS`);
     }
 
     @ON('REQUEST_BLOCKS')
@@ -113,7 +120,7 @@ export class SyncController extends BaseController {
 
     @ON('LAST_BLOCKS_UPDATE')
     updateHeaders(data: { lastBlock }): void {
-        logger.debug(`[Controller][Sync][updateHeaders]: id ${data.lastBlock.id}, height: ${data.lastBlock.height}`);
+        logger.debug(`${LOG_PREFIX}[updateHeaders]: id ${data.lastBlock.id}, height: ${data.lastBlock.height}`);
         SyncService.updateHeaders(data.lastBlock);
     }
 
