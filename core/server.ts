cf3a16825af1dbb93395d2ae367146b616752a99
Aleksandr Shevchenko <axelsheva94@gmail.com>
2019-05-17 07:46:06 +0300

    Add system information

--- core/server.ts
+++ core/server.ts
@@ -1,14 +1,23 @@
 import Loader from 'core/loader';
 import { logger } from 'shared/util/logger';
+import TransactionPool from 'core/service/transactionPool';
+import TransactionQueue from 'core/service/transactionQueue';
+import config from 'shared/config';
 
 const preconfiguration: Array<Promise<any>> = [];
 
 preconfiguration.push(Loader.start());
+
+setInterval(() => {
+    logger.debug(
+        `[Server] Queue size: ${TransactionQueue.getSize().queue}, ` +
+        `conflicred queue size: ${TransactionQueue.getSize().conflictedQueue}, ` +
+        `pool size: ${TransactionPool.getSize()}`
+    );
+}, config.CONSTANTS.UPDATE_BLOCKCHAIN_INFO_INTERVAL);
 
 Promise.all(preconfiguration)
     .then(() => {})
     .catch(err => {
         logger.error('Failed to start the server CORE. \n', err);
     });
-
-
--- core/service/events.ts
+++ core/service/events.ts
@@ -5,6 +5,8 @@ import BlockRepository from 'core/repository/block';
 import SyncService from 'core/service/sync';
 import config from 'shared/config';
 import { Address } from 'shared/model/types';
+import TransactionPool from 'core/service/transactionPool';
+import TransactionQueue from 'core/service/transactionQueue';
 
 export type BlockchainInfo = {
     totalSupply: number;
@@ -15,15 +17,20 @@ export type BlockchainInfo = {
     height: number;
     consensus: number;
     datetime: Date;
+    metrics: {
+        queue: number,
+        conflictedQueue: number,
+        pool: number,
+    },
 };
 
 class EventService {
 
     updateBlockchainInfo() {
-        const preMinedAccounts = config.CONSTANTS.PRE_MINED_ACCOUNTS.map((address: Address) => 
+        const preMinedAccounts = config.CONSTANTS.PRE_MINED_ACCOUNTS.map((address: Address) =>
             AccountRepository.getByAddress(address)
         );
-        const circulatingSupply = config.CONSTANTS.TOTAL_SUPPLY.AMOUNT - 
+        const circulatingSupply = config.CONSTANTS.TOTAL_SUPPLY.AMOUNT -
             preMinedAccounts.reduce((sum, acc) => sum += (acc ? acc.actualBalance : 0), 0);
         const statistics = AccountRepository.getStatistics();
 
@@ -36,6 +43,11 @@ class EventService {
             height: BlockRepository.getLastBlock() ? BlockRepository.getLastBlock().height : 0,
             consensus: SyncService.getConsensus(),
             datetime: new Date(),
+            metrics: {
+                queue: TransactionQueue.getSize().queue,
+                conflictedQueue: TransactionQueue.getSize().conflictedQueue,
+                pool: TransactionPool.getSize(),
+            },
         });
     }
 }
