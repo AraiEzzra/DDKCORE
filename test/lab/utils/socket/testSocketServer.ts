6c05c5c9e0bc05c9c56e52aa25e68191d56af0d3
Oleksii Fisenko <a.fisenko@sk-consulting.com.ua>
2019-06-03 12:08:55 +0300

    --no-edit

--- test/lab/utils/socket/testSocketServer.ts
+++ test/lab/utils/socket/testSocketServer.ts
@@ -1,7 +1,7 @@
 import { CONNECT_CHANNEL } from 'shared/driver/socket/channels';
 import { ISocketServer, SocketServer } from 'shared/driver/socket/server';
 import io from 'socket.io';
-import { TEST_SOCKET_SERVER_CONFIG } from 'test/lab/utils/socket/config';
+import { TEST_RUNNER_PORT, TEST_SOCKET_SERVER_CONFIG } from 'test/lab/utils/socket/config';
 import { TEST_SYNC_NAME } from 'test/lab/utils/constants';
 
 export class TestSocketServer extends SocketServer implements ISocketServer {
@@ -20,7 +20,6 @@ export class TestSocketServer extends SocketServer implements ISocketServer {
             this.socket.on(CONNECT_CHANNEL, (socket: SocketIO.Socket) => {
                 console.log(`Test node ${JSON.stringify(socket.handshake.address)} is connected to server`);
                 socket.on(TEST_SYNC_NAME, (data: any) => {
-                    // console.log(`Node: ${data.node} is synchronizing`);
                     const peerName = data.node;
                     this.socketPool.set(peerName, socket);
                     const connectionCount = this.socketPool.size;
@@ -38,11 +37,11 @@ export class TestSocketServer extends SocketServer implements ISocketServer {
         sockets.forEach((socket: any) => {
             socket.on(channel, (data: any) => {
                 listener(socket, data);
-                socket.emit(channel + '_RESPONSE', {});
+                socket.emit(channel + '_RESPONSE');
             });
         });
     }
 }
 
-export default new TestSocketServer(4000, TEST_SOCKET_SERVER_CONFIG);
+export default new TestSocketServer(TEST_RUNNER_PORT, TEST_SOCKET_SERVER_CONFIG);
 
