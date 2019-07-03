3bf59f231a453ff02a234d1492549b64f6088e7c
Aleksandr Shevchenko <axelsheva94@gmail.com>
2019-07-03 15:25:12 +0300

    Fix unhandledRejection and uncaughtException messages

--- core/server.ts
+++ core/server.ts
@@ -2,11 +2,11 @@ import Loader from 'core/loader';
 import { logger } from 'shared/util/logger';
 
 process.addListener('unhandledRejection', err => {
-    logger.error(`[Core][Process][unhandledRejection] ${JSON.stringify(err)}`);
+    logger.error(`[Core][Process][unhandledRejection] ${err}\n${err.stack}`);
 });
 
 process.addListener('uncaughtException', err => {
-    logger.error(`[Core][Process][uncaughtException] ${err.message} \n ${err.stack}`);
+    logger.error(`[Core][Process][uncaughtException] ${err}\n${err.stack}`);
 });
 
 const preconfiguration: Array<Promise<any>> = [];
