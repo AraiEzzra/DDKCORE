f28a24ce168c5735454eb7e8061c6ab4e61de8af
axelsheva <axelsheva94@gmail.com>
2019-10-18 15:13:31 +0300

    Fix deleteLastBlock method

--- core/service/block.ts
+++ core/service/block.ts
@@ -603,8 +603,8 @@ class BlockService implements IBlockService {
     }
 
     public async deleteLastBlock(): Promise<ResponseEntity<Block>> {
-        let lastBlock = BlockStorageService.getLast();
-        logger.info(`Deleting last block: ${lastBlock.id}, height: ${lastBlock.height}`);
+        const lastBlock = BlockStorageService.getLast();
+        logger.info(`[Service][Block][deleteLastBlock] ${lastBlock.id}, height: ${lastBlock.height}`);
         if (lastBlock.height === 1) {
             return new ResponseEntity<Block>({ errors: ['Cannot delete genesis block'] });
         }
@@ -615,13 +615,16 @@ class BlockService implements IBlockService {
             return new ResponseEntity({ errors: deleteResult.errors });
         }
 
+        RoundService.restoreToSlot(SlotService.getSlotNumber(lastBlock.createdAt));
         const currentRound = RoundRepository.getCurrentRound();
         currentRound.slots[lastBlock.generatorPublicKey].isForged = false;
 
         const newLastBlock = BlockStorageService.popLast();
-        const round = RoundRepository.getCurrentRound();
         const prevRound = RoundRepository.getPrevRound();
-        BlockHistoryRepository.addEvent(lastBlock, { action: BlockLifecycle.UNDO, state: { round, prevRound } });
+        BlockHistoryRepository.addEvent(
+            lastBlock,
+            { action: BlockLifecycle.UNDO, state: { round: currentRound, prevRound } },
+        );
 
         const reversedTransactions = [...lastBlock.transactions].reverse();
         for (const transaction of reversedTransactions) {
@@ -709,6 +712,15 @@ class BlockService implements IBlockService {
                 errors: validator.getLastErrors().map(err => err.message),
             });
         }
+
+        // for (const transaction of block.transactions) {
+        //     const validateResult = TransactionService.validate(transaction);
+        //     if (!validateResult.success) {
+        //         return new ResponseEntity<null>({
+        //             errors: [`Transaction '${transaction.id}' is invalid`],
+        //         });
+        //     }
+        // }
 
         if (!validateTransactionsSorting(block.transactions)) {
             return new ResponseEntity<null>({
