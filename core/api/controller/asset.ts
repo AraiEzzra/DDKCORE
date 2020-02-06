60ef535e6ead17ddf4a98b17cd90546aea661942
axelsheva <axelsheva94@gmail.com>
2020-02-06 14:59:07 +0200

    Fix asset serializer

--- core/api/controller/asset.ts
+++ core/api/controller/asset.ts
@@ -1,11 +1,12 @@
 import { ResponseEntity } from 'eska-common/dist/socket/model/response';
-import { PublicKey, VoteType } from 'ddk.registry/dist/model/common/type';
+import { PublicKey, VoteType, RawAsset } from 'ddk.registry/dist/model/common/type';
 import { Message } from 'shared/model/message';
 import { API_ACTION_TYPES } from 'shared/driver/socket/codes';
 import { API } from 'core/api/util/decorators';
 import TransactionVoteService from 'core/service/transaction/vote';
 import TransactionStakeService from 'core/service/transaction/stake';
-import { IAssetStake, IAssetVote } from 'shared/model/transaction';
+import TransactionStakeRepository from 'shared/repository/transaction/asset/stake';
+import TransactionVoteRepository from 'shared/repository/transaction/asset/vote';
 
 export class AssetController {
     constructor() {
@@ -14,7 +15,7 @@ export class AssetController {
     }
 
     @API(API_ACTION_TYPES.CREATE_STAKE_ASSET)
-    createStakeAsset(message: Message<{ address: string, amount: number }>): ResponseEntity<IAssetStake> {
+    createStakeAsset(message: Message<{ address: string, amount: number }>): ResponseEntity<RawAsset> {
         const { amount, address } = message.body;
         const trs = {
             senderAddress: BigInt(address),
@@ -22,13 +23,13 @@ export class AssetController {
         } as any;
         const asset = TransactionStakeService.create(trs);
 
-        return new ResponseEntity({ data: asset });
+        return new ResponseEntity({ data: TransactionStakeRepository.serialize(asset) });
     }
 
     @API(API_ACTION_TYPES.CREATE_VOTE_ASSET)
     createVoteAsset(
         message: Message<{ address: string, votes: Array<PublicKey>, type: VoteType }>,
-    ): ResponseEntity<IAssetVote> {
+    ): ResponseEntity<RawAsset> {
         const { votes, type, address } = message.body;
         const trs = {
             senderAddress: BigInt(address),
@@ -36,7 +37,7 @@ export class AssetController {
         } as any;
         const asset = TransactionVoteService.create(trs);
 
-        return new ResponseEntity({ data: asset });
+        return new ResponseEntity({ data: TransactionVoteRepository.serialize(asset) });
     }
 }
 
